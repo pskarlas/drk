77ba4d60726561ed8ff1e01ff8279ad711aec037
Panagiotis <panagiotis@hey.com>
2021-01-12 15:10:06 +0000

    initial commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,19 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def landlords
+  end
+
+  def tenants
+  end
+
+  def contact
+  end
+
+  def terms
+  end
+
+  def privacy
+  end
+end
